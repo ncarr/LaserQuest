8ac308eb5f9aecbc6aaf1a160b312f07e0804420
Nicholas Carr <nicholas.carr@rogers.com>
2016-12-15 09:27:18 -0500

    Added webserver test script

---
+++ webserver.js
@@ -0,0 +1,17 @@
+// This dependency is for sending data back when people try to load websites or register tags
+var express = require("express");
+var app = express();
+
+// This sets up the server to interpret tags and format pages
+app.set('view engine', 'ejs');
+// This sets up the server to work on port 8080 if you don't specify
+var port = process.env.PORT || 8080;
+
+// This routes data to the proper locations
+var router = express.Router();
+
+app.use("/console", require("./console/server"));
+app.use("/", express.static("public"));
+
+app.listen(port);
+console.log('API currently running at http://localhost:' + port);
