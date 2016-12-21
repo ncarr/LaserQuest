3956ba808c644bf82ad4feda970e2088b2da8efb
Nicholas Carr <nicholas.carr@rogers.com>
2016-12-21 11:17:30 -0500

    Auth
    Breaking changes

--- console/server.js
+++ console/server.js
@@ -1,7 +1,128 @@
 var express = require("express");
 var router = express.Router();
+var Employee = require("../app/models/employee");
+var mfa = require("node-2fa");
+var jwt = require("jsonwebtoken");
+var admin = require("firebase-admin");
+var serviceAccount = require("." + process.env.FIREBASE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DB_URL
+});
 router.route("/")
     .get(function(req, res) {
-        res.send("This will eventually be a sign-in page");
+      if (req.body) {
+        var email = req.body.email;
+      } else {
+        var email = null;
+      }
+        res.render("signin", {email: email});
     });
+router.route("/signin-challenge")
+    .post(function (req, res) {
+        Employee.findOne({ email: req.body.email }, function (err, employee) {
+            if (employee) {
+                employee.comparePassword(req.body.password, function(err, isMatch) {
+                    if (err) console.log(err);
+                    if (isMatch) {
+                        if (employee.mfa) {
+                            res.render("mfa", { mfatoken: jwt.sign({data: employee._id}, process.env.MFA_KEY, { expiresIn: "5m" }) });
+                        } else if (employee.mfa === false) {
+                            admin.auth().createCustomToken(employee._id, {type: "Employee"})
+                              .then(function(token) {
+                                res.render("console", { token: token });
+                              })
+                              .catch(function(error) {
+                                console.log("Error creating custom token:", error);
+                              });
+                        } else {
+                            var mfaSecret = mfa.generateSecret({name: 'Laser Quest', account: req.body.email});
+                            res.render("mfasetup", { mfatoken: jwt.sign({data: {id: employee._id, mfasecret: mfaSecret}}, process.env.MFA_KEY, { expiresIn: "1h" }), mfasecret: mfaSecret, delta: 0 });
+                        }
+                    } else {
+                      res.render("signin", {email: req.body.email, password: false});
+                    }
+                });
+            } else {
+                res.render("signin", {email: false});
+            }
+        });
+    });
+router.route("/mfa-challenge")
+    .post(function (req, res) {
+        jwt.verify(req.body.mfatoken, process.env.MFA_KEY, function (err, employee_id) {
+          if (err) console.log(err);
+          Employee.findById(employee_id.data, function (err, employee) {
+            if (err) console.log(err);
+            isValidCode = mfa.verifyToken(employee.mfa, req.body.code);
+            if (isValidCode) {
+                if (isValidCode.delta === 0) {
+                  admin.auth().createCustomToken(employee_id.data, {type: "Employee"})
+                    .then(function(token) {
+                      res.render("console", { token: token });
+                    })
+                    .catch(function(error) {
+                      console.log("Error creating custom token:", error);
+                    });
+                } else {
+                  res.render("mfasetup", {mfasecret: employee.mfa, delta: isValidCode.delta});
+                }
+            } else {
+              res.render("mfasetup", {mfasecret: employee.mfa, delta: null});
+            }
+          });
+        });
+    });
+  router.route("/mfa-setup-challenge")
+      .post(function (req, res) {
+          jwt.verify(req.body.mfatoken, process.env.MFA_KEY, function (err, employee_id) {
+            if (err) console.log(err);
+            console.log(employee_id.data.mfasecret);
+            console.log(employee_id.data.mfasecret.secret);
+            console.log(req.body.mfasecret);
+            isValidCode = mfa.verifyToken(employee_id.data.mfasecret.secret, req.body.code);
+            if (isValidCode) {
+                if (isValidCode.delta === 0) {
+                  Employee.findById(employee_id.data.id, function (err, employee) {
+                    if (err) console.log(err);
+                    employee.mfa = employee_id.data.mfasecret.secret;
+                    employee.save(function (err) {
+                      admin.auth().createCustomToken(employee_id.data.id, {type: "Employee"})
+                        .then(function(token) {
+                          res.render("console", { token: token });
+                        })
+                        .catch(function(error) {
+                          console.log("Error creating custom token:", error);
+                        });
+                    });
+                  });
+                } else {
+                  res.render("mfasetup", {mfatoken: jwt.sign({data: {id: employee_id.data.id, mfasecret: employee_id.data.mfasecret}}, process.env.MFA_KEY, { expiresIn: "1h" }), mfasecret: employee_id.data.mfasecret, delta: isValidCode.delta});
+                }
+            } else {
+              res.render("mfasetup", {mfatoken: jwt.sign({data: {id: employee_id.data.id, mfasecret: employee_id.data.mfasecret}}, process.env.MFA_KEY, { expiresIn: "1h" }), mfasecret: employee_id.data.mfasecret, delta: null});
+            }
+          });
+      });
+  router.route("/nophone")
+      .get(function (req, res) {
+          jwt.verify(req.body.mfatoken, process.env.MFA_KEY, function (err, employee_id) {
+            if (err) console.log(err);
+            Employee.findById(employee_id.data, function (err, emp) {
+              if (err) console.log(err);
+              employee.mfa = false;
+              employee.save(function (err) {
+                if (err) console.log(err);
+                admin.auth().createCustomToken(employee_id.data, {type: "Employee"})
+                  .then(function(token) {
+                    res.render("console", { token: token });
+                  })
+                  .catch(function(error) {
+                    console.log("Error creating custom token:", error);
+                  });
+              });
+            });
+          });
+      });
 module.exports = router;
--- readme.md
+++ readme.md
@@ -1,4 +1,18 @@
-# Laser Quest Backend
-This is the backend for a science project where we improve the Laser Quest experience.
-# Installing and running
-Clone from git, enter `MONGO_URL=mongodb://yourMongoURL` in the `.env` file, `npm install`, `npm start`
+# Laser Quest
+This is the central server for a science project where we improve the Laser Quest experience.
+## Installing and running
+1. Clone from GitHub
+2. Create a Firebase project, create a service account, make a new folder called `config` in this folder and put the JSON file for your service account's private key in there
+3. Set up your `.env` file to match this format, replacing placeholders in angle brackets:
+  ```
+  MONGO_URL=mongodb://<YOUR_MONGO_URL>
+  FIREBASE_CONFIG="var config = {apiKey: '<YOUR_FIREBASE_API_KEY', authDomain: '<YOUR_PROJECT_ID>.firebaseapp.com', databaseURL: 'https://<YOUR_PROJECT_ID>.firebaseio.com', storageBucket: '<YOUR_PROJECT_ID>.appspot.com', messagingSenderId: '<YOUR_MESSAGING_SENDER_ID>' };"
+  MFA_KEY="-----BEGIN RSA PRIVATE KEY-----<A_2048_BIT_RSA_PRIVATE_KEY>-----END RSA PRIVATE KEY-----\n"
+  FIREBASE_KEY="./config/<PATH_TO_YOUR_FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY>"
+  FIREBASE_PUBLIC_KEY="<GO_TO_YOUR_CLIENT_CERT_URL_THEN_PASTE_THE_CERT_WITH_YOUR_PRIVATE_KEY_ID_FROM_YOUR_FIREBASE_SERVICE_ACCOUNT_FILE_HERE>"
+  FIREBASE_DB_URL="https://<YOUR_PROJECT_ID>.firebaseio.com"
+  ```
+4. If you are on Windows, in an elevated PowerShell or command prompt type `npm install --global --production windows-build-tools`
+5. `npm install`
+6. To set up your first user, `npm run setup` then follow the instructions in the console
+7. To run the server, `npm start`. To use the API, you need to authenticate with the token shown after you sign in. It expires in an hour. To test just the public html pages, `npm run webserver`.
